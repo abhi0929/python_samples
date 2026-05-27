52460bbfa5a77f592c4ecd8c900d8436e147d1e7
abhi0929 <abhiramakula.07@gmail.com>
2026-05-27 11:17:33 +0530

    feat:1st upload

---
+++ basic.py
@@ -0,0 +1,84 @@
+# a=int(input())
+# b=a*1000
+# print(b,"grams")  #1st program
+
+# a=int(input())
+# b=(a*9/5)+32
+# print(b,"f")      #2nd program
+#
+# a=10
+# b=20
+# c=30
+# if(a>b and a>c):
+#     print("a is greater")
+# elif(b>c):
+#     print("b is greater")
+# else:
+#     print("c is greater")     #3rd program
+
+# a=111
+# if a<100 or a>1000:
+#     print("wrong number")
+# else:
+#     if(a%2==0):
+#         a=a%3
+#         print(a,"even number")
+#     else:
+#         a=a%2
+#         print(a,"odd number")     #4th program
+
+
+# a=101
+# if a<0 or a>100:
+#     print("invalid input")
+# elif a>91 and a<100:
+#     print("super smart")
+# elif a>81 and a<90:
+#     print("smart")
+# elif a>71 and a<80:
+#     print("smart enough")
+# elif a>61 and a<70:
+#     print("just smart")
+# elif a>36 and a<60:
+#     print("no smart")
+# else:
+#     print("dump")         #5th program
+
+
+# a=10
+# b=20
+# c=input()
+# if c=="+":
+#     c=a+b
+#     print(c)
+# elif c=="-":
+#     c=a-b
+#     print(c)
+# elif c=="*":
+#     c=a*b
+#     print(c)
+# else:
+#     c=a/b
+#     print(c)          #6th program
+
+
+# a="CVCORP"
+# for i in range(33):
+#     print(a)          #7th program
+
+# a=250
+# b=550
+# for i in range(a,b+1):
+#     if(i%11==0):
+#         print(i)          #8th program
+
+# sum=0
+# for i in range(56,153):
+#     sum+=i
+# print(sum)                #9th program
+
+# a=700
+# b=900
+# for i in range(a,b+1):
+#     if(i%2==0):
+#         print(i)          #10th program
