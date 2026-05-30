b2888e9f91c45974a2c76cb8276c835662bc1544
abhi0929 <abhiramakula.07@gmail.com>
2026-05-30 11:17:33 +0530

    feat:1st upload

---
+++ class.py
@@ -0,0 +1,20 @@
+# a = 6
+# for i in range(1,a+1):
+#     if(i%3==1):
+#         print("A",end="")
+#     elif(i%3==2):
+#         print("B",end="")
+#     else:
+#         print("C",end="")
+
+a=9
+b=25
+if(a%1==0):
+    a=a+1
+for i in range(a,b+1,4):
+    print(i)
+
+# a=10
+# b=20
+# for i in range(a,b+1,4):
+#     print(i)
